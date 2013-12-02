2621a32ac6696a3cdcb288741a9081baac08d11c
thilko <thilko.richter@googlemail.com>
2013-12-02 06:30:07 +0100

    extract builder() method

--- plugin/src/main/groovy/com/thilko/springdoc/SpringDoc.groovy
+++ plugin/src/main/groovy/com/thilko/springdoc/SpringDoc.groovy
@@ -16,8 +16,7 @@ class SpringDoc {
     }
 
     public generate(){
-        def builder = new MarkupBuilder(new PrintWriter(new File("index.html")))
-        builder.html {
+        builder().html {
             head {
                 title "Api documentation"
                 link(href:"http://netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css",rel:"stylesheet")
@@ -25,9 +24,14 @@ class SpringDoc {
             body {
                 h1 "hello, I found " + classes.size() + " classes!"
 
+
                 script(src: "http://netdna.bootstrapcdn.com/bootstrap/3.0.2/js/bootstrap.min.js")
             }
         }
+    }
+
+    private builder(){
+        return new MarkupBuilder(new PrintWriter(new File("index.html")))
     }
 
 }
