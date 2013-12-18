3fa0a2d7572c8e9c6b4a2c241efbfffae7e02624
thilko <thilko.richter@googlemail.com>
2013-12-19 00:16:02 +0100

    rename to SpringDocPLugin

--- src/main/groovy/com/thilko/springdoc/SpringDocPlugin.groovy
+++ src/main/groovy/com/thilko/springdoc/SpringDocPlugin.groovy
@@ -4,7 +4,7 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 
-class GradlePlugin implements Plugin<Project> {
+class SpringDocPlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
