7ef61a652ad56b62d9d2807b753a2030f23b0df3
thilko <thilko.richter@googlemail.com>
2013-12-28 22:07:14 +0100

    new styling

--- src/main/groovy/com/thilko/springdoc/SpringDoc.groovy
+++ src/main/groovy/com/thilko/springdoc/SpringDoc.groovy
@@ -19,73 +19,65 @@ class SpringDoc {
         builder(outFile).html {
             head {
                 title "Api documentation"
-                link(href: "http://netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css", rel: "stylesheet")
-                link(href: "springdoc.css", rel: "stylesheet")
+                link(href: "http://netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css", rel: "stylesheet", "")
+                link(href: "springdoc.css", rel: "stylesheet", "")
             }
             body {
-                div(class: "panel-group", id: "accordion") {
-                    endpoints().eachWithIndex { endpoint, idx ->
-                        div(class: "panel panel-default") {
-                            div(class: "panel-heading") {
-                                h2(class: "panel-title") {
-                                    a("data-toggle": "collapse", "data-parent": "#accordion", href: "#collapse$idx", endpoint.className())
-                                }
-                            }
-                            div(id: "collapse$idx", class: "panel-collapse collapse") {
-                                div(class: "panel-group", id: "accordion2") {
-                                    endpoint.methods().eachWithIndex { apiMethod, methodIdx ->
-                                        def apiMethodIdx = endpoint.className() + apiMethod.name()
-
-                                        div(class: "panel panel-default") {
-                                            div(class: "panel-heading") {
-                                                h3(class: "panel-title") {
-                                                    span(class: apiMethod.httpMethodCssClass(), apiMethod.httpMethod())
-                                                    span {
-                                                        a("data-toggle": "collapse", "data-parent": "#accordion2", href: "#collapse$apiMethodIdx", apiMethod.name())
+                div(class: "springdoc") {
+                    div(class: "row") {
+                        div(class: "col-md-3 sidebar") {
+                            div(class: "panel-group", id: "api-endpoint") {
+                                endpoints().eachWithIndex { endpoint, idx ->
+                                    def endpointIdx = "api-endpoint$idx"
+                                    div(class: "panel panel-default") {
+                                        div(class: "panel-heading") {
+                                            h2(class: "panel-title") {
+                                                a("data-toggle": "collapse", "data-parent": "#api-endpoint", href: "#$endpointIdx", endpoint.className())
+                                            }
+                                        }
+                                        div(id: "$endpointIdx", class: "panel-collapse collapse") {
+                                            ul(class: "list-unstyled") {
+                                                endpoint.methods().each { method ->
+                                                    li {
+                                                        a("href": "#${endpointIdx}_${method.name()}", "data-toggle": "tab", "${method.name()}")
                                                     }
-                                                    span(apiMethod.path())
                                                 }
                                             }
-                                            div(id: "collapse$apiMethodIdx", class: "panel-collapse collapse") {
-                                                ul(class: "nav nav-tabs") {
-                                                    li(class: "active") {
-                                                        a("href": "#documentation", "data-toggle": "tab", "Documentation")
-                                                    }
-                                                    li {
-                                                        a("href": "#parameter", "data-toggle": "tab", "Parameter")
-                                                    }
-                                                    li {
-                                                        a("href": "#request", "data-toggle": "tab", "Request")
-                                                    }
-                                                    li {
-                                                        a("href": "#response", "data-toggle": "tab", "Response")
-                                                    }
-                                                }
+                                        }
+                                    }
+                                }
+                            }
+                        }
 
-                                                div(class: "tab-content") {
-                                                    div(class: "tab-pane active", id: "documentation") {
-                                                        p "Lorum ipsum"
-                                                    }
-                                                    div(class: "tab-pane", id: "parameter") {
-                                                        table {
-                                                            tbody {
-                                                                apiMethod.parameter().each { param ->
-                                                                    tr {
-                                                                        td(param.name())
-                                                                    }
-                                                                }
-                                                            }
+                        div(class: "col-md-8") {
+                            div(class: "tab-content content") {
+                                endpoints().eachWithIndex { endpoint, idx ->
+                                    endpoint.methods().each { apiMethod ->
+                                        def apiMethodContent = "api-endpoint${idx}_${apiMethod.name()}"
+                                        div(class: "tab-pane", id: "$apiMethodContent") {
+                                            div(class: "well") {
+                                                h3 {
+                                                    span(class: apiMethod.httpMethodCssClass(), apiMethod.httpMethod())
+                                                    span apiMethod.path()
+                                                    span apiMethod.name()
+                                                }
+                                            }
+                                            table(class: "table table-hover") {
+                                                caption "Parameters"
+                                                thead {
+                                                    th "Name"
+                                                    th "Required"
+                                                    th "Default"
+                                                }
+                                                tbody {
+                                                    apiMethod.parameter().each { param ->
+                                                        tr {
+                                                            td param.name()
+                                                            td param.required()
+                                                            td param.defaultValue()
                                                         }
-
-                                                    }
-                                                    div(class: "tab-pane", id: "request") {
-                                                        p "Lorum ipsum"
-                                                    }
-                                                    div(class: "tab-pane", id: "response") {
-                                                        p "Lorum ipsum"
                                                     }
                                                 }
-
                                             }
                                         }
                                     }
@@ -94,6 +86,7 @@ class SpringDoc {
                         }
                     }
                 }
+
 
                 script(src: "http://code.jquery.com/jquery-1.10.1.min.js", "")
                 script(src: "http://code.jquery.com/jquery-migrate-1.2.1.min.js", "")
