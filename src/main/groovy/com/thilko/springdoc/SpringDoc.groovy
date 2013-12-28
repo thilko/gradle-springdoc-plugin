61e9a66a257dfc132547cb7e859072fb7b7e8c8e
thilko <thilko.richter@googlemail.com>
2013-12-28 06:58:58 +0100

    removed test stuff

--- src/main/groovy/com/thilko/springdoc/SpringDoc.groovy
+++ src/main/groovy/com/thilko/springdoc/SpringDoc.groovy
@@ -94,20 +94,6 @@ class SpringDoc {
                         }
                     }
                 }
-                div(class: "container") {
-                    div(class: "row") {
-                        div(class: "col-md-1") {
-                            ul(class: "nav nav-pills nav-stacked") {
-                                li(class: "active") {
-                                    a(href: "#", "test")
-                                }
-                                li {
-                                    a(href: "#", "hallo")
-                                }
-                            }
-                        }
-                    }
-                }
 
                 script(src: "http://code.jquery.com/jquery-1.10.1.min.js", "")
                 script(src: "http://code.jquery.com/jquery-migrate-1.2.1.min.js", "")
