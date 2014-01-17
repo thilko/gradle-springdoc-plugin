f76abaf9005c19716adeaeea1ba200ead90436c2
thilko <thilko.richter@googlemail.com>
2014-01-17 02:45:11 +0100

    introduce an own configuration to add the plugin itself to the classpath

--- src/main/groovy/com/thilko/springdoc/SpringDocPlugin.groovy
+++ src/main/groovy/com/thilko/springdoc/SpringDocPlugin.groovy
@@ -11,10 +11,18 @@ class SpringDocPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
         project.plugins.apply("java")
+        if (project.configurations.findByName("springdoc")==null){
+            project.configurations.create("springdoc")
+        }
+
+        project.dependencies {
+            springdoc "com.thilko.spring:gradle-springdoc-plugin:0.1.3"
+            springdoc localGroovy()
+        }
 
         project.task(type: Copy, "copyCss") {
             project.afterEvaluate {
-                from project.zipTree(project.configurations.compile.filter { it.name.startsWith('gradle-springdoc-plugin') }.singleFile)
+                from project.zipTree(project.configurations.springdoc.filter { it.name.startsWith('gradle-springdoc-plugin') }.singleFile)
                 include "springdoc.css"
                 into project.buildDir
             }
@@ -23,7 +31,7 @@ class SpringDocPlugin implements Plugin<Project> {
         project.task(type: JavaCompile, "generateSpringDoc", dependsOn: project.copyCss) {
             project.afterEvaluate {
                 source = it.sourceSets.main.java
-                classpath = it.sourceSets.main.output + project.configurations.compile
+                classpath = it.sourceSets.main.output + project.configurations.compile + project.configurations.springdoc
 
                 options.compilerArgs = [
                         "-s", "${project.buildDir}",
