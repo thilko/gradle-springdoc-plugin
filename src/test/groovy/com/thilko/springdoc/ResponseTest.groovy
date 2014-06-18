2490f37dc21bf696953e1790a5557467310a8b21
thilko <thilko.richter@googlemail.com>
2014-06-18 21:41:52 +0200

    test refactorinh

--- src/test/groovy/com/thilko/springdoc/ResponseTest.groovy
+++ src/test/groovy/com/thilko/springdoc/ResponseTest.groovy
@@ -15,35 +15,35 @@ class ResponseTest extends Specification {
 
     def "responseClass returns 'String' if method returns a string"() {
         when:
-        def resource = Controller.createController(compiler.customerController())
+        def resourceGroups = Controller.withController(compiler.customerController())
 
         then:
-        resource.methods[4].response().className() == "java.lang.String"
+        resourceGroups[0].resources[4].response().className() == "java.lang.String"
     }
 
     def "responseClass returns 'com.thilko.springdoc.User' if method returns a User"() {
         when:
-        def resource = Controller.createController(compiler.customerController())
+        def resourceGroups = Controller.withController(compiler.customerController())
 
         then:
-        resource.methods[2].response().className() == "com.thilko.springdoc.User"
+        resourceGroups[0].resources[2].response().className() == "com.thilko.springdoc.User"
     }
 
 
     def "responseClass returns 'void' if method returns nothing"() {
         when:
-        def resource = Controller.createController(compiler.customerController())
+        def resourceGroups = Controller.withController(compiler.customerController())
 
         then:
-        resource.methods[3].response().className() == "void"
+        resourceGroups[0].resources[3].response().className() == "void"
     }
 
     def "responseAsJson for ResponseEntity<InformationResponse> returns json for InformationResponse"(){
         when:
-        def resource = Controller.createController(compiler.customerController())
+        def resourceGroups = Controller.withController(compiler.customerController())
 
         then:
-        resource.methods[8].response().asJson() ==
+        resourceGroups[2].resources[0].response().asJson() ==
                 """{
     "valid": true
 }"""
@@ -51,24 +51,23 @@ class ResponseTest extends Specification {
 
     def "responseAsJson returns json"() {
         when:
-        def resource = Controller.createController(compiler.customerController())
+        def resourceGroups = Controller.withController(compiler.customerController())
 
         then:
-        resource.methods[2].response().asJson() ==
+        resourceGroups[0].resources[2].response().asJson() ==
                 """{
     "firstName": null,
     "id": null,
     "lastName": null,
     "dateOfBirth": null
 }"""
-
     }
 
     def "responseAsJson returns '' when response is void"() {
         when:
-        def resource = Controller.createController(compiler.customerController())
+        def resourceGroups = Controller.withController(compiler.customerController())
 
         then:
-        resource.methods[3].response().asJson() == ""
+        resourceGroups[0].resources[3].response().asJson() == ""
     }
 }
